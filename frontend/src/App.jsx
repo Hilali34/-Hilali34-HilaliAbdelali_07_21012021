9dfb0d41bdbfd5c2c7acec7abc60f446634c1202
Hilali <Abdelali>
2021-08-23 04:28:13 +0200

    initialisation partie front projet 7

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React, {Component, useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import NavBar from './components/NavBar';
+
+class App extends Component{
+
+   render(){
+      return (
+        <div className="App">
+            <NavBar/>
+        </div>
+      );
+   }
+}
+
+
+
+export default App
